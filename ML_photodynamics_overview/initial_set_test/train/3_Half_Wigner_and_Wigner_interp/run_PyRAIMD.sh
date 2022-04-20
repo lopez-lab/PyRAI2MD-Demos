e4d1e365e509a5863ff1f18532c21588b2cf0424
Li <lijingbai2009@gmail.com>
2022-04-20 17:53:23 -0400

    upload

---
+++ ML_photodynamics_overview/initial_set_test/train/3_Half_Wigner_and_Wigner_interp/run_PyRAIMD.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=23:00:00
+#SBATCH --job-name=hfb-train
+#SBATCH --partition=short
+#SBATCH --mem=30Gb
+#SBATCH --output=%j.o.slurm
+#SBATCH --error=%j.e.slurm
+#SBATCH --constraint="ib"
+
+export INPUT=input
+export WORKDIR=/scratch/lijingbai2009/ChemAccRes/training_curves/train/3_Half_Wigner_and_Wigner_interp
+export PYRAIMD=/home/lijingbai2009/share/NN-ChemI/PyRAIMD/bin
+export PATH=/work/lopez/Python-3.7.4/bin:$PATH
+
+cd $WORKDIR
+python3 $PYRAIMD/PyRAIMD.py $INPUT
+
